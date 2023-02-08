56a4d4a587005c16cf1d0fa202248f6590b1476c
Lukas Holzer <lukas.holzer@netlify.com>
2023-02-08 11:33:29 +0100

    chore: updates

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    legacyBrowsers: false,
+    newNextLinkBehavior: true,
+    fallbackNodePolyfills: false,
+    nextScriptWorkers: true, // see: https://nextjs.org/docs/basic-features/script#off-loading-scripts-to-a-web-worker-experimental
+  },
   swcMinify: true,
   reactStrictMode: false,
 };
