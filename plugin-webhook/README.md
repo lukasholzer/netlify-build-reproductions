457a6c4eac5e2629a6e4f849952664703360268e
Lukas Holzer <lukas.holzer@netlify.com>
2023-02-16 15:14:56 +0100

    chore: update

---
+++ plugin-webhook/README.md
@@ -0,0 +1,5 @@
+# how to trigger preview build
+
+```bash
+curl -X POST -d {"preview":true} https://api.netlify.com/build_hooks/63ee36d0ff790200629ce9dd
+```
--- plugin-webhook/plugin-webhook-data/index.js
+++ plugin-webhook/plugin-webhook-data/index.js
@@ -4,10 +4,25 @@ import { NetlifyAPI } from 'netlify';
 export const onPreBuild = async ({
   constants: { NETLIFY_API_TOKEN, SITE_ID },
 }) => {
+  if (!process.env.INCOMING_HOOK_BODY) {
+    return;
+  }
+
   const api = new NetlifyAPI(NETLIFY_API_TOKEN);
+  const response = await api.updateSiteDeploy({
+    site_id: SITE_ID,
+    deploy_id: DEPLOY_ID,
+    body: {
+      draft: true,
+    },
+  });
 
+  console.log(response);
+  // const data = await new Response(decodeURI('%7Bpreview:true%7D')).();
+  // console.log(data);
+  // console.log(eval(decodeURI('%7Bpreview:true%7D')));
   // Fetch sites
-  // const sites = await api.updateSiteDeploy(SITE_ID);
 
-  console.log(`onPreBuild`, SITE_ID, process.env);
+  // console.log(`onPreBuild`, SITE_ID, process.env);
+  // console.log('body:', process.env.INCOMING_HOOK_BODY);
 };
