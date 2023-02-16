5306a9b301ae64896003164f4225bf720a24d540
Lukas Holzer <lukas.holzer@netlify.com>
2023-02-16 14:56:54 +0100

    chore: update

--- plugin-webhook/plugin-webhook-data/index.js
+++ plugin-webhook/plugin-webhook-data/index.js
@@ -1,5 +1,13 @@
-module.exports = {
-  onPreBuild: async ({ utils }) => {
-    console.log(`onPreBuild`);
-  },
+import { NetlifyAPI } from 'netlify';
+
+/** @type {import('@netlify/build').OnPreBuild} */
+export const onPreBuild = async ({
+  constants: { NETLIFY_API_TOKEN, SITE_ID },
+}) => {
+  const api = new NetlifyAPI(NETLIFY_API_TOKEN);
+
+  // Fetch sites
+  // const sites = await api.updateSiteDeploy(SITE_ID);
+
+  console.log(`onPreBuild`, SITE_ID, process.env);
 };
