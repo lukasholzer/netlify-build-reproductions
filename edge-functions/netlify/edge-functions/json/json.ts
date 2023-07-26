ca1cf6c70235798a51396caed5c41138a1401dfe
Lukas Holzer <lukas.holzer@netlify.com>
2023-07-26 11:53:23 +0200

    chore: different log

--- edge-functions/netlify/edge-functions/json/json.ts
+++ edge-functions/netlify/edge-functions/json/json.ts
@@ -1,6 +1,12 @@
 import type { Context } from 'https://edge.netlify.com';
 
 export default async (request: Request, context: Context) => {
-  console.log(`${new Date().getTime()} – ${context.geo.city}`);
+  console.log(
+    `${new Date().getTime()} – ${context.geo.city} ${JSON.stringify(
+      context,
+      null,
+      2
+    )}`
+  );
   return Response.json({ hello: 'world', location: context.geo.city });
 };
