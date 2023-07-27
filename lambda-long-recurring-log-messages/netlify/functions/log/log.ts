47e8749ad1e9038e2522b17c43e7d12d9c636628
Lukas Holzer <lukas.holzer@netlify.com>
2023-07-27 10:58:07 +0200

    chore: actually log something

--- lambda-long-recurring-log-messages/netlify/functions/log/log.ts
+++ lambda-long-recurring-log-messages/netlify/functions/log/log.ts
@@ -1,6 +1,7 @@
 import { Handler } from '@netlify/functions';
 
 export const handler: Handler = async (event, context) => {
+  console.log(JSON.stringify({ event, context }, null, 2));
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }, null, 2),
